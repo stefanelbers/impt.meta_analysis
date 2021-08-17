73b07cfed45d6f0dc0c2e9d7995ed15e072066e9
stefanelbers <stefanelbers@hotmail.com>
2021-08-17 18:15:30 +0200

    debug prep_ft_assessment: problem with code "ER - " did not work, because one abstract contained the string "lower - ".

--- deployment/prep_ft_assessment_table.R
+++ deployment/prep_ft_assessment_table.R
@@ -1,4 +1,4 @@
-data_fulltext <- read_bibliography("second_round_selection.txt")
+data_fulltext <- read_bibliography("deployment/second_round_selection.txt")
 
 t_fulltext <- select(data_fulltext, author, year, title, LB, n1)
 
@@ -12,7 +12,7 @@ names(t_fulltext)[names(t_fulltext) == "n1"] <- "comment"
 
 #create vector with study ids of included studies
 included_id <- c(1, 9, 12, 14, 16, 30, 36, 46, 48, 51, 55, 58, 69, 90, 92, 96, 101, 102, 110, 113, 119, 123,
-              132, 133, 134, 138, 139, 144, 147, 148, 149, 152, 154, 164, 166, 167, 180, 181, 183, 184, 185, 
+              132, 133, 134, 138, 139, 144, 147, 148, 149, 152, 154, 157, 164, 166, 167, 180, 181, 183, 184, 185, 
               204, 211, 212, 223, 228, 246, 266, 269, 271, 277, 284, 290, 303, 306, 317, 325, 335, 340, 350,
               354, 356, 360, 364, 375
               )
